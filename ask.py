4b782102c7ca2799a6347a7713af6fc77921083f
YEfeEnhos <91611406+YEfeEnhos@users.noreply.github.com>
2023-09-19 23:13:04 +0300

    Update ask.py

--- ask.py
+++ ask.py
@@ -255,7 +255,7 @@ def main():
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
-	    uploaded()
+		uploaded()
             with st.spinner("Processing"):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
